eeea4a211bc98b3b895b3a28d3a23ac22f859093
Wei Liu <imneway@gmail.com>
2025-11-14 21:14:40 +0800

    Added icon indicator for the turbo mode.

--- src/android/app/src/main/java/org/citra/citra_emu/utils/TurboHelper.kt
+++ src/android/app/src/main/java/org/citra/citra_emu/utils/TurboHelper.kt
@@ -5,16 +5,31 @@
 package org.citra.citra_emu.utils
 
 import android.widget.Toast
+import java.util.concurrent.CopyOnWriteArraySet
 import org.citra.citra_emu.CitraApplication
 import org.citra.citra_emu.NativeLibrary
 import org.citra.citra_emu.R
 import org.citra.citra_emu.features.settings.model.IntSetting
 
 object TurboHelper {
+    fun interface TurboStateListener {
+        fun onTurboStateChanged(isEnabled: Boolean)
+    }
+
     private var turboSpeedEnabled = false
+    private val turboStateListeners = CopyOnWriteArraySet<TurboStateListener>()
 
     fun isTurboSpeedEnabled(): Boolean {
         return turboSpeedEnabled
+    }
+
+    fun registerListener(listener: TurboStateListener) {
+        turboStateListeners.add(listener)
+        listener.onTurboStateChanged(turboSpeedEnabled)
+    }
+
+    fun unregisterListener(listener: TurboStateListener) {
+        turboStateListeners.remove(listener)
     }
 
     fun reloadTurbo(showToast: Boolean) {
@@ -35,11 +50,19 @@ object TurboHelper {
     }
 
     fun setTurboEnabled(state: Boolean, showToast: Boolean) {
+        val changed = turboSpeedEnabled != state
         turboSpeedEnabled = state
         reloadTurbo(showToast)
+        if (changed) {
+            notifyTurboStateChanged()
+        }
     }
 
     fun toggleTurbo(showToast: Boolean) {
         setTurboEnabled(!TurboHelper.isTurboSpeedEnabled(), showToast)
+    }
+
+    private fun notifyTurboStateChanged() {
+        turboStateListeners.forEach { it.onTurboStateChanged(turboSpeedEnabled) }
     }
 }
